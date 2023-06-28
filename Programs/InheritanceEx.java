a104216349c4ae794c6a0a766ee7f71e2cc09e0b
Shashank <96179184+get002@users.noreply.github.com>
2023-06-28 17:36:41 +0530

    Add files via upload

--- Programs/InheritanceEx.java
+++ Programs/InheritanceEx.java
@@ -1,8 +1,14 @@
 
 public class InheritanceEx {
 public static void main(String[] args) {
-	dog d=new dog();
-	d.move("fast");
+	Animal a1=new Animal("HUge",400);
+	System.out.println(a1);
+	//a1.bark();
+	
+	dog d1=new dog("small",50,"bat ear","curly");
+	System.out.println(d1);
+	d1.bark();
+	d1.move("fast");
 }
 }
 
@@ -10,7 +16,9 @@ public static void main(String[] args) {
 class Animal{		//parent class, base class, super class
 	public String size;
 	public double weight;
-	
+	Animal(){
+		
+	}
 	public Animal(String size,double weight) {
 		this.size=size;
 		this.weight=weight;
@@ -24,14 +32,38 @@ class Animal{		//parent class, base class, super class
 	public void eat() {
 		System.out.println("Animal eats something");
 	}
-	
+	public String toString() {
+		return "Animal Size:"+size+" weight: "+weight;
+	}
 }
 
 
 class dog extends Animal{ //derived class , child class, sub class
+	public String earShape;
+	public String tailShape;
 	
-	dog(String size,double weight){
+	public dog(String size,double weight,String earShape,String tailShape) {
 		super(size,weight);
+		this.earShape=earShape;
+		this.tailShape=tailShape;
 	}
+	public void bark() {
+		System.out.println("Dog is barking");
+	}
+	public void run() {
+		System.out.println("Dog is running");
+	}
+	
+	public String toString() {
+		return super.toString()+" tail Shape:"+tailShape+" earShape: "+earShape;
+	}
+	
+	public void move(String speed,int kmph) {
+		System.out.println("Dog moves "+speed);
+	}
+	public void move(String speed) {
+		System.out.println("Dog moves "+speed);
+	}
+	
 	
 }
